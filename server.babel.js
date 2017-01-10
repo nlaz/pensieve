4ddec9c9a49da6c12c5538bc9307a840f604f90d
nlaz <nikolazaris@utexas.edu>
2017-01-09 22:47:37 -0500

    setup two apis

--- server.babel.js
+++ server.babel.js
@@ -1,8 +1,27 @@
 import express from 'express';
 import { Item } from './db';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.get('/api/items', (req, res) => {
+	Item.find({}, (err, items) => {
+		if (err) { console.log(err); }
+		return res.send(items);
+	});
+});
+
+app.get('/api/items/:itemId', (req, res) => {
+	Item.findById(req.params.itemId, (err, item) => {
+		if (err) { console.log(err); }
+		return res.send(item);
+	});
+});
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
@@ -12,7 +31,4 @@ app.listen(port, () => {
 		console.log(res);
 		console.log(res.length);
 	});
-
-	const item = new Item({ value: 'test' });
-	item.save();
 });
