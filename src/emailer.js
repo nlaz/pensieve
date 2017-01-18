cb501cc8c423c6e9c3c2b712fdb80a44f70b6423
nlaz <nikolazaris@utexas.edu>
2017-01-17 22:18:02 -0500

    add target name

--- src/emailer.js
+++ src/emailer.js
@@ -6,7 +6,7 @@ const sourceEmail = new helper.Email('boreas@test.com');
 const subject = 'Your Daily Review - Boreas';
 
 const constructEmailRequest = (targetName, targetEmail, items) => {
-	const content = new helper.Content('text/plain', `These are the items for you to review today:\n\n${items}\n\nSee you tomorrow,\nBoreas`);
+	const content = new helper.Content('text/plain', `Hi ${targetName},\n\nThese are the items for you to review today:\n\n${items}\n\nSee you tomorrow,\nBoreas`);
 	const mail = new helper.Mail(sourceEmail, subject, new helper.Email(targetEmail), content);
 	return sendgrid.emptyRequest({
 		method: 'POST',
