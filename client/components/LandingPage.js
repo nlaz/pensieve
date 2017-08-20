f04ab35b629dfb8ab9b42f7eae124f2b4f93bfd8
nlaz <nikolazaris@utexas.edu>
2017-08-20 17:53:03 -0400

    update signup form validation and style

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -3,30 +3,21 @@ import Header from './Header';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as emailActions from '../actions/emailActions';
+import { SignupForm } from './SignupForm';
 const HEADER_IMAGE = require('../assets/images/illustration.png');
 
 export class LandingPage extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { email: '' };
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
+		this.onSignup = this.onSignup.bind(this);
 	}
 
-	onChange(e) {
-		const { value } = e.target;
-		this.setState(() => ({ email: value }));
-	}
-
-	onSubmit(e) {
-		e.preventDefault();
-		const { email } = this.state;
+	onSignup(email) {
 		this.props.actions.prelaunchSignUp({ email });
 	}
 
 	render() {
-		const { email } = this.state;
 		const { isSuccess, hasErrored } = this.props;
 		return (
 			<Header className='landing-page text-center'>
@@ -35,23 +26,11 @@ export class LandingPage extends React.Component {
 						<h4 className='coming-soon'>COMING SOON</h4>
 						<h1 className='title'>Cut your study time in half</h1>
 						<h4 className='subtitle'><strong>Learn smarter.</strong> Boreas uses intelligent flashcards to find the best time for you to review so you learn better.</h4>
-						<form className='landing-form' onSubmit={this.onSubmit}>
-							{isSuccess &&
-								<div className='info-success'>Success! Thanks for signing up to our magical product.</div>
-							}
-							{!isSuccess &&
-								<div>
-		              <label htmlFor='email'>Beta coming early September. Request an invite.</label>
-									<div className='landing-input'>
-										<input onChange={this.onChange} name='email' type='text' placeholder='Email Address' value={email} />
-										<button onClick={this.onSubmit} type='submit' className='btn btn-primary'>Request Access</button>
-									</div>
-									{hasErrored &&
-										<div className='input-error'>Sorry! You broke our thing :(</div>
-									}
-								</div>
-							}
-						</form>
+						<SignupForm
+							isSuccess={isSuccess}
+							hasErrored={hasErrored}
+							onSignup={this.onSignup}
+						/>
 						</div>
 					<div className='landing-image col-sm-6'>
 						<img alt='Hero image' src={HEADER_IMAGE} />
