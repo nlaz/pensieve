c53eae53d77e3c28b41bafaa909b5779574c74ad
nlaz <nikolazaris@utexas.edu>
2017-08-22 22:32:48 -0400

    update deck style and copy

--- client/components/items/ItemsContainer.js
+++ client/components/items/ItemsContainer.js
@@ -9,7 +9,7 @@ import * as itemActions from '../../actions/itemActions';
 
 export const PAGE_SIZE = 24;
 
-export const PageNavigation = ({ numPages, onDecrementPage, onIncrementPage, onChangePage}) => (
+export const PageNavigation = ({ numPages, onDecrementPage, onIncrementPage, onChangePage }) =>
 	<nav aria-label="Page navigation" style={{ textAlign: 'center' }}>
 		<ul className="pagination">
 			<li>
@@ -18,9 +18,13 @@ export const PageNavigation = ({ numPages, onDecrementPage, onIncrementPage, onC
 				</a>
 			</li>
 
-			{Array(numPages).fill(1).map((obj, key) => (
-				<li key={key} onClick={() => onChangePage(key)}><a href="#">{key}</a></li>
-			))}
+			{Array(numPages).fill(1).map((obj, key) =>
+				<li key={key} onClick={() => onChangePage(key)}>
+					<a href="#">
+						{key}
+					</a>
+				</li>
+			)}
 
 			<li>
 				<a onClick={onIncrementPage} href="#" aria-label="Next">
@@ -28,61 +32,71 @@ export const PageNavigation = ({ numPages, onDecrementPage, onIncrementPage, onC
 				</a>
 			</li>
 		</ul>
-	</nav>
-);
+	</nav>;
 
-const ProgressBar = ({ progress }) => (
-	<div className='progress'>
-	<div className='progress-bar progress-bar-info'
-	role='progressbar'
-	aria-valuenow={progress}
-	aria-valuemin='0'
-	aria-valuemax='100'
-	style={{ width: `${progress}%` }}
-	>
-	<span className="sr-only">{progress}% Complete</span>
-	</div>
-	</div>
-);
+const ProgressBar = ({ progress }) =>
+	<div className="progress">
+		<div
+			className="progress-bar progress-bar-info"
+			role="progressbar"
+			aria-valuenow={progress}
+			aria-valuemin="0"
+			aria-valuemax="100"
+			style={{ width: `${progress}%` }}
+		>
+			<span className="sr-only">
+				{progress}% Complete
+			</span>
+		</div>
+	</div>;
 
 export const ItemCard = ({ item, className }) => {
 	const maxTime = Math.max(moment(item.nextReviewDate).diff(item.updatedAt, 'hours'), 0);
 	const progressTime = Math.max(moment(item.nextReviewDate).diff(moment(), 'hours'), 0);
-	const progress = (( progressTime / maxTime) * 100) || 0;
+	const progress = progressTime / maxTime * 100 || 0;
 
 	return (
 		<div className={`itemCard-wrapper ${className}`}>
-			<Link to={`/items/${item._id}`} className='itemCard'>
-			<ProgressBar progress={progress} />
-			<h5 style={{ margin: '0', fontSize: '16px' }}>{item.title}</h5>
-			{item.hidden &&
-				<div className='hideIcon'>
-				<span className='glyphicon glyphicon-eye-close' aria-hidden='true' ></span>
-				</div>}
-				</Link>
+			<Link to={`/items/${item._id}`} className="itemCard">
+				<ProgressBar progress={progress} />
+				<h5 style={{ margin: '0', fontSize: '16px' }}>
+					{item.title}
+				</h5>
+				{item.hidden &&
+					<div className="hideIcon">
+						<span className="glyphicon glyphicon-eye-close" aria-hidden="true" />
+					</div>}
+			</Link>
 		</div>
-		);
+	);
 };
 
 export const PageHeader = ({ count, onSearchChange }) => {
 	return (
-		<div className='page-header'>
-		<div className='info'>
-		<h4 className='title'>Items</h4>
-		{count &&
-			<p className='subtitle'>{count} items in your collection</p>
-		}
-		</div>
-		<div className='actions'>
-		<div className='search'>
-		<input onChange={onSearchChange} type='text' id='search' className='form-control' placeholder='Search for items...' />
-		</div>
-		<div className='create'>
-		<Link to='/items/new' className='btn-newItem btn pull-right'>
-		New Item +
-		</Link>
-		</div>
-		</div>
+		<div className="page-header">
+			<div className="info">
+				<h4 className="title">Items</h4>
+				{count &&
+					<p className="subtitle">
+						{count} items in your collection
+					</p>}
+			</div>
+			<div className="actions">
+				<div className="search">
+					<input
+						onChange={onSearchChange}
+						type="text"
+						id="search"
+						className="form-control"
+						placeholder="Search for items..."
+					/>
+				</div>
+				<div className="create">
+					<Link to="/items/new" className="btn-newItem btn pull-right">
+						Create Item +
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
@@ -131,30 +145,32 @@ class ItemsContainer extends React.Component {
 
 		if (!items) {
 			return (
-				<div className='col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2'>
-				<h4 className='page-header'>Your Items</h4>
-				<h4>You don't have any items yet. <em>Ahem. Bummer...</em></h4>
+				<div className="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+					<h4 className="page-header">Your Items</h4>
+					<h4>
+						You don't have any items yet. <em>Ahem. Bummer...</em>
+					</h4>
 				</div>
 			);
 		}
 
-		const filteredItems = filter.length > 0 ? items.filter(item => item.title.toLowerCase().indexOf(filter.toLowerCase()) !== -1) : items;
+		const filteredItems =
+			filter.length > 0
+				? items.filter(item => item.title.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
+				: items;
 		const numPages = Math.ceil(filteredItems.length / PAGE_SIZE);
 		const pageStart = activePage * PAGE_SIZE;
 		const pageEnd = Math.min((activePage + 1) * PAGE_SIZE, filteredItems.length);
 		const pageItems = filteredItems.slice(pageStart, pageEnd);
 
 		return (
-			<Header className='items-page'>
-				<div className='container'>
-					<PageHeader
-						count={items.length}
-						onSearchChange={this.onSearchChange}
-					/>
-					<div className='row'>
-						{pageItems.map((item, key) => (
-							<ItemCard className='col-xs-6 col-sm-3 col-lg-2' item={item} key={key} />
-						))}
+			<Header className="items-page">
+				<div className="container">
+					<PageHeader count={items.length} onSearchChange={this.onSearchChange} />
+					<div className="row">
+						{pageItems.map((item, key) =>
+							<ItemCard className="col-xs-6 col-sm-3 col-lg-2" item={item} key={key} />
+						)}
 					</div>
 					{numPages > 1 &&
 						<PageNavigation
@@ -162,19 +178,18 @@ class ItemsContainer extends React.Component {
 							onIncrementPage={this.onIncrementPage}
 							onDecrementPage={this.onDecrementPage}
 							onChangePage={this.onChangePage}
-						/>
-					}
+						/>}
 				</div>
 			</Header>
 		);
 	}
 }
 
-const mapStateToProps = (state) => ({
-	items: state.data.items,
+const mapStateToProps = state => ({
+	items: state.data.items
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
 	actions: bindActionCreators(itemActions, dispatch)
 });
 
