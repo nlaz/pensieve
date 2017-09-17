deb8e946a09339cabf3bca27aed903e7ca679f94
nlaz <nikolazaris@utexas.edu>
2017-09-17 10:38:00 -0400

    update meta tag verification

--- server/middleware.js
+++ server/middleware.js
@@ -11,49 +11,44 @@ import appReducer from '../client/reducers/appReducer';
 const BUNDLE_URL = `${process.env.HOST_URL}/bundle.js`;
 
 const renderFullPage = (appHtml, preloadedState) =>
-	`
+  `
 	<!doctype html public='storage'>
 	<html>
 	<head>
 		<meta charset='utf-8'>
 		<meta content='width=device-width, initial-scale=1' name='viewport'/>
-		<meta name="google-site-verification" content="UsghB5oUWO4R7iGRIyloNyUg393dawgDoQdbF6QXHF0" />
+    <meta name="google-site-verification" content="Rfy9OvqmaP8cjCqP3BseC2B-r9ByVwgjexH8XpkUlG4" />
 		<title>Pensieve</title>
 		<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'/>
 	</head>
 	<body>
 		<div id='root'>${appHtml}</div>
-		<script>window.INITIAL_STATE=${JSON.stringify(preloadedState).replace(
-			/</g,
-			'\\u003c'
-		)}</script>
+		<script>window.INITIAL_STATE=${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}</script>
 		<script src='${BUNDLE_URL}' type='text/javascript'></script>
 	</body>
 	</html>
 	`;
 
 export default (req, res) => {
-	match({ routes: routes, location: req.url }, (err, redirect, props) => {
-		if (err) {
-			res.status(500).send(err.message);
-		} else if (redirect) {
-			res.redirect(redirect.pathname + redirect.search);
-		} else if (props) {
-			const createStoreWithMiddleware = applyMiddleware(reduxThunk)(
-				createStore
-			);
-			const store = createStoreWithMiddleware(appReducer);
-			const html = renderToString(
-				<Provider store={store}>
-					<RouterContext {...props} />
-				</Provider>
-			);
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else if (redirect) {
+      res.redirect(redirect.pathname + redirect.search);
+    } else if (props) {
+      const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+      const store = createStoreWithMiddleware(appReducer);
+      const html = renderToString(
+        <Provider store={store}>
+          <RouterContext {...props} />
+        </Provider>
+      );
 
-			const preloadedState = store.getState();
+      const preloadedState = store.getState();
 
-			res.send(renderFullPage(html, preloadedState));
-		} else {
-			res.status(404).send('Not Found');
-		}
-	});
+      res.send(renderFullPage(html, preloadedState));
+    } else {
+      res.status(404).send('Not Found');
+    }
+  });
 };
