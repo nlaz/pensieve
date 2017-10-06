61b9f986790ebc8650862165280db7bd416c2260
nlaz <nikolazaris@utexas.edu>
2017-10-06 19:36:13 -0400

    pass items back from createDeck endpoint

--- server/controllers/decks.js
+++ server/controllers/decks.js
@@ -2,26 +2,28 @@ import Deck from '../models/deck';
 import Item from '../models/item';
 
 export async function getDecks(req, res) {
-	try {
-		const decks = await Deck.find({ user_id: req.user._id });
-		return res.status(200).json({ decks });
-	} catch (error) {
-		return res.status(404).json({ error });
-	}
+  try {
+    const decks = await Deck.find({ user_id: req.user._id });
+    return res.status(200).json({ decks });
+  } catch (error) {
+    return res.status(404).json({ error });
+  }
 }
 
 export async function getDeck(req, res) {
-	const deckId = req.params.deck_id;
-	const userId = req.user._id;
+  const deckId = req.params.deck_id;
+  const userId = req.user._id;
 
-	try {
-		const deck = await Deck.findOne({ _id: deckId, user_id: userId });
-		deck.items = await Item.find().where('_id').in(deck.items);
+  try {
+    const deck = await Deck.findOne({ _id: deckId, user_id: userId });
+    deck.items = await Item.find()
+      .where('_id')
+      .in(deck.items);
 
-		return res.status(200).json({ deck });
-	} catch (error) {
-		return res.status(404).json({ error });
-	}
+    return res.status(200).json({ deck });
+  } catch (error) {
+    return res.status(404).json({ error });
+  }
 }
 
 /**
@@ -29,36 +31,31 @@ export async function getDeck(req, res) {
  * Constructs item objects after building a deck.
  */
 export async function createDeck(req, res) {
-	if (req.body.items && req.body.items.length === 0) {
-		return res.status(404).json({ error: 'Decks must have at least one item' });
-	}
+  if (req.body.items && req.body.items.length === 0) {
+    return res.status(404).json({ error: 'Decks must have at least one item' });
+  }
 
-	try {
-		const deck = await new Deck({
-			user_id: req.user._id,
-			title: req.body.title,
-			description: req.body.description
-		}).save();
+  try {
+    const deck = await new Deck({
+      user_id: req.user._id,
+      title: req.body.title,
+      description: req.body.description
+    }).save();
 
-		const itemObjs = req.body.items.map(item => ({
-			...item,
-			user_id: req.user._id,
-			deck_id: deck._id
-		}));
+    const itemObjs = req.body.items.map(item => ({
+      ...item,
+      user_id: req.user._id,
+      deck_id: deck._id
+    }));
 
-		const items = await Item.create(itemObjs);
-		const itemIds = items.map(item => item._id);
+    const items = await Item.create(itemObjs);
 
-		const newDeck = await Deck.findOneAndUpdate(
-			{ _id: deck._id },
-			{ items: itemIds },
-			{ new: true }
-		);
+    const newDeck = await Deck.findOneAndUpdate({ _id: deck._id }, { items: items }, { new: true });
 
-		return res.status(200).json({ deck: newDeck });
-	} catch (error) {
-		return res.status(404).json({ error });
-	}
+    return res.status(200).json({ deck: newDeck });
+  } catch (error) {
+    return res.status(404).json({ error });
+  }
 }
 
 /**
@@ -66,34 +63,38 @@ export async function createDeck(req, res) {
  * Constructs item objects after building a deck.
  */
 export async function editDeck(req, res) {
-	const deckId = req.params.deck_id;
-	const userId = req.user._id;
+  const deckId = req.params.deck_id;
+  const userId = req.user._id;
 
-	try {
-		let deck = await Deck.findOne({ _id: deckId, user_id: userId });
-		await Item.remove().where('_id').in(deck.items);
+  try {
+    let deck = await Deck.findOne({ _id: deckId, user_id: userId });
+    await Item.remove()
+      .where('_id')
+      .in(deck.items);
 
-		const itemObjs = req.body.items.map(item => ({
-			...item,
-			user_id: req.user._id,
-			deck_id: deck._id
-		}));
+    const itemObjs = req.body.items.map(item => ({
+      ...item,
+      user_id: req.user._id,
+      deck_id: deck._id
+    }));
 
-		const items = await Item.create(itemObjs);
-		const itemIds = items.map(item => item._id);
+    const items = await Item.create(itemObjs);
+    const itemIds = items.map(item => item._id);
 
-		deck = await Deck.findOneAndUpdate(
-			{ _id: deck._id },
-			{ items: itemIds, title: req.body.title, description: req.body.description },
-			{ new: true }
-		);
+    deck = await Deck.findOneAndUpdate(
+      { _id: deck._id },
+      { items: itemIds, title: req.body.title, description: req.body.description },
+      { new: true }
+    );
 
-		deck.items = await Item.find().where('_id').in(deck.items);
+    deck.items = await Item.find()
+      .where('_id')
+      .in(deck.items);
 
-		return res.status(200).json({ deck });
-	} catch (error) {
-		return res.status(404).json({ error });
-	}
+    return res.status(200).json({ deck });
+  } catch (error) {
+    return res.status(404).json({ error });
+  }
 }
 
 /**
@@ -101,11 +102,11 @@ export async function editDeck(req, res) {
  * the deck as well.
  */
 export async function deleteDeck(req, res) {
-	try {
-		const items = await Item.remove({ deck_id: req.params.deck_id });
-		const deck = await Deck.remove({ _id: req.params.deck_id });
-		return res.status(200).json({ deck, items });
-	} catch (error) {
-		return res.status(404).json({ error });
-	}
+  try {
+    const items = await Item.remove({ deck_id: req.params.deck_id });
+    const deck = await Deck.remove({ _id: req.params.deck_id });
+    return res.status(200).json({ deck, items });
+  } catch (error) {
+    return res.status(404).json({ error });
+  }
 }
