9ac9b77d3e95013ff8edbc2e4664f4f6165d8628
nlaz <nikolazaris@utexas.edu>
2017-12-08 14:41:16 -0500

    fix spacing on not found page

--- client/components/pages/NotFoundPage.jsx
+++ client/components/pages/NotFoundPage.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from "react";
+import { Link } from "react-router";
 
-import Footer from '../footer';
-import PageTemplate from './PageTemplate';
+import Footer from "../footer";
+import PageTemplate from "./PageTemplate";
 
 export default function NotFoundPage() {
   return (
-    <PageTemplate footer={<Footer />}>
-      <div className="col-md-8 offset-md-2 text-center">
-        <span style={{ fontSize: '80px', fontWeight: 'bold' }}>4😅4</span>
-        <h3 style={{ marginBottom: '40px' }}>Oops, something went wrong.</h3>
-        <Link to="/" className="btn btn-primary">
-          Go Home
-        </Link>
+    <PageTemplate className="not-found-page pt-5" footer={<Footer />}>
+      <div className="container mt-5">
+        <div className="col-md-8 offset-md-2 text-center">
+          <span style={{ fontSize: "80px", fontWeight: "bold" }}>4😅4</span>
+          <h3 style={{ marginBottom: "40px" }}>Oops, something went wrong.</h3>
+          <Link to="/" className="btn btn-primary">
+            Go Home
+          </Link>
+        </div>
       </div>
     </PageTemplate>
   );
