e52a1522c62f14c3ca4be453cd10227ccd4e66fb
nlaz <nikolazaris@utexas.edu>
2017-12-11 16:34:01 -0500

    migrate item ids from strings to objectids

--- server/models/review.js
+++ server/models/review.js
@@ -5,8 +5,9 @@ const Schema = mongoose.Schema;
 const ReviewSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    item: { type: Schema.Types.ObjectId, ref: "Item", required: true },
     user_id: { type: String }, // deprecated
-    item_id: { type: String, required: true },
+    item_id: { type: String }, // deprecated
     value: { type: String },
   },
   { timestamps: true },
